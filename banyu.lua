d33a477225526a6c74c429dbb2adf00a03276aab
AnonymousJava01 <rikkachan12123@gmail.com>
2025-02-27 17:41:27 +0700

    Update banyu.lua

--- banyu.lua
+++ banyu.lua
@@ -523,7 +523,33 @@ local Options = Fluent.Options
 
 
     -- // Exclusives Tab // --
-    
+    Tabs.Exclusives:AddButton({
+            Title = "Hope Old Server",
+            Description = "",
+            Callback = function()
+                -- Fisch Old Server Finder --
+local httprequest = http_request or request
+local PlaceId = game.PlaceId
+local HttpService = game:GetService("HttpService")
+local servers = {}
+local req = httprequest({Url = string.format("https://games.roblox.com/v1/games/%d/servers/Public?sortOrder=Desc&limit=100&excludeFullGames=true", 16732694052)})
+local body = HttpService:JSONDecode(req.Body)
+
+if body and body.data then
+    for i, v in next, body.data do
+        if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= game.JobId then
+            table.insert(servers, 1, v.id)
+        end
+    end
+end
+print(#servers)
+for i=1, #servers do
+    pcall(function()
+        game:GetService("TeleportService"):TeleportToPlaceInstance(16732694052, servers[i], game.Players.LocalPlayer)
+    end)
+end
+            end
+        })
     -- // Main Tab // --
     
     -- // // // Auto Cast // // // --
